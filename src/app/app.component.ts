e1cfa49172f5155edd3ae33526412fb4655a4c7e
Adam <Adammajcher20@gmail.com>
2022-05-10 12:57:22 +0200

    remove map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,4 @@ declare var ol: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'erasmus-website';
-  latitude: number = 20;
-  longitude: number = 74;
-  map: any;
-  ngOnInit() {
-    this.map = new ol.Map({
-      target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM(),
-        }),
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([73.8567, 18.5204]),
-        zoom: 8,
-      }),
-    });
-  }
-}
+export class AppComponent {}
