6c33a699b9403e111cdb732bc8cf859b5ab1b367
Adam <Adammajcher20@gmail.com>
2022-05-25 23:22:46 +0200

    update uczestnicy

--- components/students-grid.js
+++ components/students-grid.js
@@ -1,21 +1,21 @@
 import Image from 'next/image'
 
 const students = [
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
-  { name: 'Student', image: '/test.png' },
+  { name: 'Kuba Fuchs', image: '/portrety/Portret (1).jpg' },
+  { name: 'Michał Dydyk', image: '/portrety/Portret (2).jpg' },
+  { name: 'Kuba Bysiewicz', image: '/portrety/Portret (3).jpg' },
+  { name: 'Karol Kuzyk', image: '/portrety/Portret (4).jpg' },
+  { name: 'Błażej Ryba', image: '/portrety/Portret (5).jpg' },
+  { name: 'Mariusz Pociask', image: '/portrety/Portret (6).jpg' },
+  { name: 'Sabina Pelc', image: '/portrety/Portret (7).jpg' },
+  { name: 'Hubert Woźniak', image: '/portrety/Portret (8).jpg' },
+  { name: 'Jan Pieprzycki', image: '/portrety/Portret (9).jpg' },
+  { name: 'Marcin Wiercioch', image: '/portrety/Portret (10).jpg' },
+  { name: 'Szymon Łukarski', image: '/portrety/Portret (11).jpg' },
+  { name: 'Kacper Turek', image: '/portrety/Portret (12).jpg' },
+  { name: 'Aleksandra Kiełb', image: '/portrety/Portret (13).jpg' },
+  { name: 'Stanisław Karnas', image: '/portrety/Portret (14).jpg' },
+  { name: 'Adam Majcher', image: '/portrety/Portret (15).jpg' },
 ]
 
 const StudentsGrid = () => {
--- pages/uczestnicy.js
+++ pages/uczestnicy.js
@@ -9,7 +9,8 @@ export default function Home() {
         title="Uczestnicy"
         text1="Jesteśmy grupą 15 osobową z 5 szkół wchodzących w konsorcjum którego
         organizatorem jest Edu-It w Rzeszowie. W skład naszego konsorcjum wchodzą:"
-        text2=""
+        text2="Zespół Szkół nr 2 im. Jana Kochanowskiego w Łańcucie, Zespół Szkół Elektronicznych w Rzeszowie, Zespół Szkół im. gen. Józefa Kustonia w Lubaczowie, Zespół Szkół Technicznych w Łańcucie, Zespół Szkół Energetycznych w Rzeszowie
+        "
         image="/Image6.png"
       />
       <StudentsGrid />
